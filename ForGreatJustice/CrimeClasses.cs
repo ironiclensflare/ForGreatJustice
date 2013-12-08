6c4a2b00ec6ba71cdd8a490fc87f6f12fadc8016
Stu <stu@ironiclensflare.com>
2013-12-08 11:47:57 +0000

    Setup

---
+++ ForGreatJustice/CrimeClasses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForGreatJustice
+{
+    public class Crime
+    {
+        public string category { get; set; }
+        public Location location { get; set; }
+        public DateTime month { get; set; }
+        public string persistent_id { get; set; }
+    }
+
+    public class Location
+    {
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public Street street { get; set; }
+    }
+
+    public class Street
+    {
+        public string name { get; set; }
+    }
+
+    public class OutcomeStatus
+    {
+        public string category { get; set; }
+        public DateTime date { get; set; }
+    }
+}
